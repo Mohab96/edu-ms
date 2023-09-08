68d82c434d2a70998db8c4280f7e0a92254c7eae
Mohab Yaser <mohabkhaleefa540@gmail.com>
2023-09-09 00:49:35 +0200

    Fixing some bugs in models.py

--- edu/main/models.py
+++ edu/main/models.py
@@ -10,20 +10,17 @@ class BaseUser(models.Model):
 
 
 class Student(BaseUser):
-    cart = models.OneToOneField('Cart', on_delete=models.CASCADE)
-    courses = models.ManyToManyField('Course', null=True, blank=True)
-    wishlist = models.ManyToManyField('Course', null=True, blank=True)
+    courses = models.ManyToManyField('Course')
+    wishlist = models.ManyToManyField('Course')
 
 
 class Course(models.Model):
-    instructor = models.ForeignKey(
-        'Instructor', on_delete=models.CASCADE, related_name='instructor')
     name = models.CharField(max_length=250)
     description = models.TextField()
     reviews_count = models.IntegerField()
     students_count = models.IntegerField()
     created_by = models.ForeignKey(
-        'Instructor', on_delete=models.PROTECT, related_name='instructor')
+        'Instructor', on_delete=models.PROTECT, related_name='courses')
     last_update = models.DateField(auto_now=True)
     requirements = models.TextField(null=True, blank=True)
     objectives = models.TextField(null=True, blank=True)
@@ -40,7 +37,7 @@ class Category(models.Model):
 
 class Review(models.Model):
     instructor = models.ForeignKey(
-        'Instructor', on_delete=models.PROTECT, related_name='instructor')
+        'Instructor', on_delete=models.PROTECT, related_name='reviews')
 
     course = models.ForeignKey(
         Course, on_delete=models.CASCADE, related_name='reviews')
@@ -50,6 +47,7 @@ class Review(models.Model):
 
     # TODO: how to make PROTECT while the at least
     # one of the three fields is still there?
+
     rating = models.DecimalField(max_digits=2, decimal_places=2)
     body = models.TextField()
 
@@ -58,7 +56,7 @@ class Instructor(BaseUser):
     courses_count = models.IntegerField()
     rating = models.DecimalField(max_digits=2, decimal_places=2)
     reviews_count = models.IntegerField()
-    students = models.ManyToManyField(Student, blank=True, null=True)
+    students = models.ManyToManyField(Student)
     students_count = models.IntegerField()
     bio = models.TextField(null=True, blank=True)
 
@@ -66,14 +64,14 @@ class Instructor(BaseUser):
 class Cart(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4)
     student = models.ForeignKey(
-        Student, on_delete=models.CASCADE, related_name='student')
+        Student, on_delete=models.CASCADE, related_name='cart')
     courses = models.ManyToManyField(Course)
     total_price = models.IntegerField()
 
 
 class CourseContent(models.Model):
     course = models.OneToOneField(
-        Course, on_delete=models.CASCADE, related_name='content')
+        Course, related_name='content')
 
 
 class Section(models.Model):
@@ -85,6 +83,7 @@ class MaterialItem(models.Model):
     section = models.ForeignKey(
         Section, on_delete=models.CASCADE, related_name='items')
     name = models.CharField(max_length=250)
+
     # file TODO
 
 
@@ -97,6 +96,6 @@ class Question(models.Model):
 
 
 class Answer(models.Model):
-    question = models.ForeignKey(
-        Question, models.CASCADE, related_name='answer')
+    question = models.OneToOneField(
+        Question, related_name='answer')
     body = models.CharField(max_length=250)
